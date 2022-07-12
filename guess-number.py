48cc38fa2bb29b83457d298174c687dc991a6b36
Diya Karmakar <professionaldiya734@gmail.com>
2022-07-12 20:13:42 +0530

    Guess a number updated

--- guess-number.py
+++ guess-number.py
@@ -1,11 +1,12 @@
 import random
 
+# A function to check whether a number guessed by a user is correct or not
 def guess(x,y):
     random_number = random.randint(x,y)
     guess_number = 0
 
     while(random_number!=guess_number):
-        guess_number = int(input(f"Guess a number between {x} and {y}: 3"))
+        guess_number = int(input(f"Guess a number between {x} and {y}: "))
         if guess_number > random_number:
             print("Guess lower")
         elif guess_number < random_number:
@@ -13,6 +14,7 @@ def guess(x,y):
 
     print("You guessed it right!")
 
+# A function to check whether a number guessed by a computer is correct or not
 def computer_guess(x):
     low = 1
     high = x
@@ -24,14 +26,14 @@ def computer_guess(x):
         else:
             guess = low
         feedback = input(f"Is {guess} too high(H), too low(L) or correct(C): ").lower()
-        if guess == 'h':
+        if feedback == 'h':
             high = guess-1
-        elif guess == 'l':
-            lower = guess+1
+        elif feedback == 'l':
+            low = guess+1
 
     
     print(f"Yay! The computer your number {guess}, correctly!")
 
 
 
-computer_guess(10)
+computer_guess(1000)
